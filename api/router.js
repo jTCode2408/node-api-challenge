0c369a9d87a888972a77210e058246e647d87026
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-14 17:23:41 -0500

    routes for actions done in actionRouter

--- api/router.js
+++ api/router.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const projectRouter = require('./projectRouter')
+const projectRouter = require('./projectRouter');
+const actionRouter = require ('./actionRouter');
 
 const router = express.Router();
 
 
 router.use('/projects', projectRouter);
+router.use('/actions', actionRouter)
 
 
 module.exports = router;
