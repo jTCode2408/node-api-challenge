90a1c1161dfef6f67269a98af5fcabf5b7eab5ba
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-14 17:07:32 -0500

    routes for PJ's done in PJrouter

---
+++ api/router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const projectRouter = require('./projectRouter')
+
+const router = express.Router();
+
+
+router.use('/projects', projectRouter);
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const server = express();
-
+const router = require('./api/router')
 
 server.use(express.json());
+server.use('/api', router)
 
 
 server.get('/', (req, res) => {
